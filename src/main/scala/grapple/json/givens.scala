7f146b26012d9b3605f8abc36e52656f9d44aee6
Carlos Conyers <carlos.conyers@hotmail.com>
2026-04-27 20:37:52 -0400

    Updated givens scaladoc comments

--- src/main/scala/grapple/json/givens.scala
+++ src/main/scala/grapple/json/givens.scala
@@ -22,7 +22,7 @@ import scala.util.{ Failure, Try }
 /**
  * Returns `JsonValue` as is.
  *
- * This instance is required to perform actions such as the following:
+ * This is required to perform actions such as the following:
  *
  * {{{
  * import scala.language.implicitConversions
@@ -37,158 +37,174 @@ import scala.util.{ Failure, Try }
  */
 given jsonValueJsonInput: JsonInput[JsonValue] = identity(_)
 
-/** Casts `JsonValue` to `JsonNull`. */
+/** Casts JSON value to `JsonNull`. */
 given jsonNullJsonInput: JsonInput[JsonNull.type] = expect(_)
 
-/** Casts `JsonValue` to `JsonString`. */
+/** Casts JSON value to `JsonString`. */
 given jsonStringJsonInput: JsonInput[JsonString] = expect(_)
 
-/** Casts `JsonValue` to `JsonBoolean`. */
+/** Casts JSON value to `JsonBoolean`. */
 given jsonBooleanJsonInput: JsonInput[JsonBoolean] = expect(_)
 
-/** Casts `JsonValue` to `JsonNumber`. */
+/** Casts JSON value to `JsonNumber`. */
 given jsonNumberJsonInput: JsonInput[JsonNumber] = expect(_)
 
-/** Casts `JsonValue` to `JsonObject`. */
+/** Casts JSON value to `JsonObject`. */
 given jsonObjectJsonInput: JsonInput[JsonObject] = expect(_)
 
-/** Casts `JsonValue` to `JsonArray`. */
+/** Casts JSON value to `JsonArray`. */
 given jsonArrayJsonInput: JsonInput[JsonArray] = expect(_)
 
-/** Converts `JsonValue` to `String`. */
+/** Provides JSON input for `String`. */
 given stringJsonInput: JsonInput[String] = expect[JsonString](_).value
 
-/** Converts `String` to `JsonString`. */
+/** Provides JSON output for `String`. */
 given stringJsonOutput: JsonOutput[String] = JsonString(_)
 
-/** Converts `JsonValue` to `Boolean`. */
+/** Provides JSON input for `Boolean`. */
 given booleanJsonInput: JsonInput[Boolean] = expect[JsonBoolean](_).value
 
-/** Converts `Boolean` to `JsonBoolean`. */
+/** Provides JSON output for `Boolean`. */
 given booleanJsonOutput: JsonOutput[Boolean] = JsonBoolean(_)
 
-/** Converts `JsonValue` to `Int`. */
+/** Provides JSON input for `Int`. */
 given intJsonInput: JsonInput[Int] = expect[JsonNumber](_).toInt
 
-/** Converts `Int` to `JsonNumber`. */
+/** Provides JSON output for `Int`. */
 given intJsonOutput: JsonOutput[Int] = JsonNumber(_)
 
-/** Converts `JsonValue` to `Long`. */
+/** Provides JSON input for `Long`. */
 given longJsonInput: JsonInput[Long] = expect[JsonNumber](_).toLong
 
-/** Converts `Long` to `JsonNumber`. */
+/** Provides JSON output for `Long`. */
 given longJsonOutput: JsonOutput[Long] = JsonNumber(_)
 
-/** Converts `JsonValue` to `Float`. */
+/** Provides JSON input for `Float`. */
 given floatJsonInput: JsonInput[Float] = expect[JsonNumber](_).toFloat
 
-/** Converts `Float` to `JsonNumber`. */
+/** Provides JSON output for `Float`. */
 given floatJsonOutput: JsonOutput[Float] = JsonNumber(_)
 
-/** Converts `JsonValue` to `Double`. */
+/** Provides JSON input for `Double`. */
 given doubleJsonInput: JsonInput[Double] = expect[JsonNumber](_).toDouble
 
-/** Converts `Double` to `JsonNumber`. */
+/** Provides JSON output for `Double`. */
 given doubleJsonOutput: JsonOutput[Double] = JsonNumber(_)
 
-/** Converts `JsonValue` to `BigInt`. */
+/** Provides JSON input for `BigInt`. */
 given bigIntJsonInput: JsonInput[BigInt] = expect[JsonNumber](_).toBigInt
 
-/** Converts `BigInt` to `JsonNumber`. */
+/** Provides JSON output for `BigInt`. */
 given bigIntJsonOutput: JsonOutput[BigInt] = JsonNumber(_)
 
-/** Converts `JsonValue` to `BigDecimal`. */
+/** Provides JSON input for `BigDecimal`. */
 given bigDecimalJsonInput: JsonInput[BigDecimal] = expect[JsonNumber](_).toBigDecimal
 
-/** Converts `BigDecimal` to `JsonNumber`. */
+/** Provides JSON output for `BigDecimal`. */
 given bigDecimalJsonOutput: JsonOutput[BigDecimal] = JsonNumber(_)
 
-/** Converts `JsonValue` to `Option`. */
+/**
+ * Provides JSON input for `Option[T]`.
+ *
+ * @note `JsonNull` translates to `None`.
+ */
 given optionJsonInput[T](using input: JsonInput[T]): JsonInput[Option[T]] =
   case JsonNull => None
   case value    => Some(input.read(value))
 
-/** Converts `Some` to `JsonValue` or returns `JsonNull` if `None`. */
+/**
+ * Provides JSON output for `Option[T]`.
+ *
+ * @note `None` translates to `JsonNull`.
+ */
 given optionJsonOutput[T, C[T] <: Option[T]](using output: JsonOutput[T]): JsonOutput[C[T]] =
   _.fold(JsonNull)(output.write(_))
 
-/** Converts `None` to `JsonNull`. */
+/** Provides JSON output for `None`. */
 given noneJsonOutput: JsonOutput[None.type] = _ => JsonNull
 
-/** Converts `JsonValue` to `Try`. */
+/**
+ * Provides JSON input for `Try[T]`.
+ *
+ * @note `JsonNull` translates to `Failure`.
+ */
 given tryJsonInput[T](using input: JsonInput[T]): JsonInput[Try[T]] =
   case JsonNull => Failure(NullPointerException())
   case value    => Try(input.read(value))
 
-/** Converts `Success` to `JsonValue` or returns `JsonNull` if `Failure`. */
+/**
+ * Provides JSON output for `Try[T]`.
+ *
+ * @note `Failure` translates to `JsonNull`.
+ */
 given tryJsonOutput[T, C[T] <: Try[T]](using output: JsonOutput[T]): JsonOutput[C[T]] =
   _.fold(_ => JsonNull, output.write(_))
 
-/** Converts `Failure` to `JsonNull`. */
+/** Provides JSON output for `Failure[?]`. */
 given failureJsonOutput: JsonOutput[Failure[?]] = _ => JsonNull
 
-/** Converts `JsonValue` to `Either`. */
+/** Provides JSON input for `Either[A, B]`. */
 given eitherJsonInput[A, B](using left: JsonInput[A])(using right: JsonInput[B]): JsonInput[Either[A, B]] =
   value => Try(Right(right.read(value))).getOrElse(Left(left.read(value)))
 
-/** Converts `Either` to `JsonValue`. */
+/** Provides JSON output for `Either[A, B]`. */
 given eitherJsonOutput[A, B, C[A, B] <: Either[A, B]](using left: JsonOutput[A])(using right: JsonOutput[B]): JsonOutput[C[A, B]] =
   _.fold(left.write(_), right.write(_))
 
-/** Converts `Right` to `JsonValue`. */
+/** Provides JSON output for `Right[?, T]`. */
 given rightJsonOutput[T](using output: JsonOutput[T]): JsonOutput[Right[?, T]] =
   _.fold(_ => JsonNull, output.write(_))
 
-/** Converts `Left` to `JsonValue`. */
+/** Provides JSON output for `Left[?, T]`. */
 given leftJsonOutput[T](using output: JsonOutput[T]): JsonOutput[Left[T, ?]] =
   _.fold(output.write(_), _ => JsonNull)
 
-/** Converts `JsonValue` to `Array`. */
+/** Provides JSON input for `Array[T]`. */
 given arrayJsonInput[T](using input: JsonInput[T])(using ctag: ClassTag[T]): JsonInput[Array[T]] =
   expect[JsonArray](_).values.map(input.read).toArray
 
-/** Converts `Array` to `JsonArray`. */
+/** Provides JSON output for `Array[T]`. */
 given arrayJsonOutput[T](using output: JsonOutput[T]): JsonOutput[Array[T]] =
   _.foldLeft(JsonArrayBuilder()) {
     (builder, value) => builder.add(output.write(value))
   }.toJsonArray()
 
-/** Converts `JsonValue` to `Iterable`. */
+/** Provides JSON input for `Iterable[T]`. */
 given iterableJsonInput[T, C[T] <: Iterable[T]](using input: JsonInput[T])(using factory: Factory[T, C[T]]): JsonInput[C[T]] =
   expect[JsonArray](_).values.foldLeft(factory.newBuilder) {
     (builder, value) => builder += input.read(value)
   }.result
 
-/** Converts `Iterable` to `JsonArray`. */
+/** Provides JSON output for `Iterable[T]`. */
 given iterableJsonOutput[T, C[T] <: Iterable[T]](using output: JsonOutput[T]): JsonOutput[C[T]] =
   _.foldLeft(JsonArrayBuilder()) {
     (builder, value) => builder.add(output.write(value))
   }.toJsonArray()
 
-/** Converts `JsonValue` to `Map`. */
+/** Provides JSON input for `Map[String, T]`. */
 given mapJsonInput[T, C[T] <: Map[String, T]](using input: JsonInput[T])(using factory: Factory[(String, T), C[T]]): JsonInput[C[T]] =
   expect[JsonObject](_).fields.foldLeft(factory.newBuilder) {
     case (builder, (key, value)) => builder += key -> input.read(value)
   }.result
 
-/** Converts `Map` to `JsonObject`. */
+/** Provides JSON output for `Map[String, T]`. */
 given mapJsonOutput[T, C[T] <: Map[String, T]](using output: JsonOutput[T]): JsonOutput[C[T]] =
   _.foldLeft(JsonObjectBuilder()) {
     case (builder, (key, value)) => builder.add(key, output.write(value))
   }.toJsonObject()
 
-/** Applies conversion using `JsonInput`. */
+/** Applies JSON input conversion. */
 given jsonInputConversion[T](using input: JsonInput[T]): Conversion[JsonValue, T] =
   input.read(_)
 
-/** Applies conversion using `JsonOutput`. */
+/** Applies JSON output conversion. */
 given jsonOutputConversion[T](using output: JsonOutput[T]): Conversion[T, JsonValue] =
   output.write(_)
 
-/** Converts `(String, T)` to `(String, JsonValue)`. */
-given jsonFieldConversion[T](using output: JsonOutput[T]): Conversion[(String, T), (String, JsonValue)] =
+/** Applies JSON output conversion to field. */
+given fieldsJsonOutputConversion[T](using output: JsonOutput[T]): Conversion[(String, T), (String, JsonValue)] =
   _ -> output.write(_)
 
-/** Converts `JsonValue` to `JsonStructureFacade`. */
+/** Converts JSON value to `JsonStructureFacade`. */
 given jsonStructureFacadeConversion: Conversion[JsonValue, JsonStructureFacade] =
   json => JsonStructureFacade(expect(json))
